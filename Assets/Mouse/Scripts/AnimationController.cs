28eec579e5855d3710e78fc78736b418a8bb7629
Jay Jackson <jaynickjackson@gmail.com>
2021-01-15 23:54:41 -0500

    Update movement / refactor animation management

---
+++ Assets/Mouse/Scripts/AnimationController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationController : MonoBehaviour
+{
+    Animator anim;
+    [SerializeField] bool lockCursor = true;
+    private bool isJumping = false;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        if(lockCursor)
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+    }
+
+    void Update()
+    {
+        float horizontal = (
+            Mathf.Abs(ThirdPersonMovement.velocity.normalized.x) +
+            Mathf.Abs(ThirdPersonMovement.velocity.normalized.z)
+        ) / 2;
+        anim.SetBool("Walk", (horizontal > 0));
+        anim.SetBool("Run", horizontal > 0 && Input.GetKey(KeyCode.LeftShift));
+    }
+}
