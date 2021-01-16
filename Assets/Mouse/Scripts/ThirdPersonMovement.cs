88746387862a0711baa50a9ebf4167328a1fe1b2
Jay Jackson <jaynickjackson@gmail.com>
2021-01-16 00:52:18 -0500

    Run animation implemented

--- Assets/Mouse/Scripts/ThirdPersonMovement.cs
+++ Assets/Mouse/Scripts/ThirdPersonMovement.cs
@@ -14,9 +14,11 @@ public class ThirdPersonMovement : MonoBehaviour
 
     void Update()
     {
-        float horizontal = Input.GetAxisRaw("Horizontal");
-        float vertical = Input.GetAxisRaw("Vertical");
-        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+        Vector3 direction = new Vector3(
+            Input.GetAxisRaw("Horizontal"),
+            0f,
+            Input.GetAxisRaw("Vertical")
+        ).normalized;
 
         if(direction.magnitude >= 0.1f) {
             float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
